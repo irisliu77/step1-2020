0e4ad6aaf7cc6564e9da032ac054f568051d99f5
jiayiliu <jiayiliu@live.unc.edu>
2020-07-07 18:55:57 -0400

    factor out common code

--- src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/StackOverflowClient.java
+++ src/main/java/com/google/step/YOUR_PROJECT_NAME_HERE/external/StackOverflowClient.java
@@ -4,15 +4,14 @@ import com.google.step.YOUR_PROJECT_NAME_HERE.data.Card;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.*;
-import java.util.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public final class StackOverflowClient {
@@ -41,28 +40,12 @@ public final class StackOverflowClient {
       return cards;
     }
     for (int i = 0; i < so_urls.size(); i++) {
-      CloseableHttpClient httpClient = HttpClients.createDefault();
       try {
         URI uri = new URI(so_urls.get(i));
-        System.out.println("path = " + uri.getPath());
         String[] segments = uri.getPath().split("/");
         String idStr = segments[2];
         String url = String.format(SEARCH_URL_TEMPLATE, idStr);
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
-        if (response.getStatusLine().getStatusCode() != 200) {
-          return cards;
-        }
-        HttpEntity entity = response.getEntity();
-        if (entity == null) {
-          return cards;
-        }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
-        StringBuilder responseBody = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-          responseBody.append(line);
-        }
-        JSONObject res = new JSONObject(responseBody.toString());
+        JSONObject res = getResponse(url);
         String title = res.getJSONArray("items").getJSONObject(0).get("title").toString();
         Card c = new Card();
         c.setLink(uri.toString());
@@ -79,29 +62,11 @@ public final class StackOverflowClient {
   private List<Card> getAnswersId(List<Card> cards) {
     for (int i = 0; i < DEFAULT_ANSWER_AMOUNT; i++) {
       String url = String.format(QUESTION_URL_TEMPLATE, cards.get(i).getCode());
-      CloseableHttpClient httpClient = HttpClients.createDefault();
       try {
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
-        if (response.getStatusLine().getStatusCode() != 200) {
-          return cards;
-        }
-        HttpEntity entity = response.getEntity();
-        if (entity == null) {
-          return cards;
-        }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
-        StringBuilder responseBody = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-          responseBody.append(line);
-        }
-        JSONObject res = new JSONObject(responseBody.toString());
-        JSONArray answer_list = res.getJSONArray("items");
+        JSONObject res = getResponse(url);
         String answer_id = res.getJSONArray("items").getJSONObject(0).get("answer_id").toString();
         cards.get(i).setCode(answer_id);
-      } catch (ClientProtocolException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
@@ -111,31 +76,34 @@ public final class StackOverflowClient {
   private List<Card> getAnswers(List<Card> cards) {
     for (int i = 0; i < DEFAULT_ANSWER_AMOUNT; i++) {
       String url = String.format(ANSWER_URL_TEMPLATE, cards.get(i).getCode());
-      CloseableHttpClient httpClient = HttpClients.createDefault();
       try {
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
-        if (response.getStatusLine().getStatusCode() != 200) {
-          return cards;
-        }
-        HttpEntity entity = response.getEntity();
-        if (entity == null) {
-          return cards;
-        }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
-        StringBuilder responseBody = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-          responseBody.append(line);
-        }
-        JSONObject res = new JSONObject(responseBody.toString());
+        JSONObject res = getResponse(url);
         String body = res.getJSONArray("items").getJSONObject(0).get("body").toString();
         cards.get(i).setCode(body);
-      } catch (ClientProtocolException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
     return cards;
+  }
+
+  private JSONObject getResponse(String url) throws IOException {
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
+    if (response.getStatusLine().getStatusCode() != 200) {
+      return new JSONObject();
+    }
+    HttpEntity entity = response.getEntity();
+    if (entity == null) {
+      return new JSONObject();
+    }
+    BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+    StringBuilder responseBody = new StringBuilder();
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      responseBody.append(line);
+    }
+    JSONObject res = new JSONObject(responseBody.toString());
+    return res;
   }
 }
